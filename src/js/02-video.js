5b76fc024dc97bc85cc99a2aa693708743cc0a46
YPV0 <yuriisfw@gmail.com>
2023-04-27 03:19:28 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,11 +1,16 @@
 import Player from '@vimeo/player';
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
 
-player.on('play', function () {
-  console.log('played the video!');
-});
+player.on('timeupdate', updateTime);
 
-player.getVideoTitle().then(function (title) {
-  console.log('title:', title);
-});
+function updateTime() {
+  player.getCurrentTime(function (seconds) {
+    localStorage.setItem('videoplayer-current-time', seconds);
+  });
+}
+
+const savedTime = localStorage.getItem('videoplayer-current-time');
+if (savedTime !== null) {
+  player.setCurrentTime(savedTime);
+}
