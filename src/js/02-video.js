a4338b9c23f952d0d753c66b8899840246f836ac
YPV0 <yuriisfw@gmail.com>
2023-04-27 03:44:56 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,11 +1,13 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.on('timeupdate', updateTime);
+player.on('timeupdate', throttle(updateTime, 1000));
 
 function updateTime() {
-  player.getCurrentTime(function (seconds) {
+  player.getCurrentTime().then(seconds => {
     localStorage.setItem('videoplayer-current-time', seconds);
   });
 }
